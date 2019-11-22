739ccb5c4573faeb83026c2cba3b178d6d71c84c
NyCko93 <46218650+NyCko93@users.noreply.github.com>
2019-11-22 08:49:51 +0100

    Add contains in help and about activity

--- app/src/main/java/com/fossourier/nicolas/mynews/Controllers/Activities/AboutActivity.java
+++ app/src/main/java/com/fossourier/nicolas/mynews/Controllers/Activities/AboutActivity.java
@@ -1,16 +1,48 @@
 package com.fossourier.nicolas.mynews.Controllers.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
 
 import com.fossourier.nicolas.mynews.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        configureToolBar();
     }
+
+    //-------------------//
+    // Configure Toolbar //
+    //-------------------//
+    private void configureToolBar() {
+        setSupportActionBar(mToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        assert ab != null;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Handle back click to close menu
+            super.onBackPressed();
+        }
+
 }
+
--- app/src/main/java/com/fossourier/nicolas/mynews/Controllers/Activities/HelpActivity.java
+++ app/src/main/java/com/fossourier/nicolas/mynews/Controllers/Activities/HelpActivity.java
@@ -1,16 +1,43 @@
 package com.fossourier.nicolas.mynews.Controllers.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
 
 import com.fossourier.nicolas.mynews.R;
 
+import java.util.Properties;
+
+import butterknife.BindView;
+
 public class HelpActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        configureToolBar();
+    }
+
+    //-------------------//
+    // Configure Toolbar //
+    //-------------------//
+    private void configureToolBar() {
+        setSupportActionBar(mToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        assert ab != null;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Handle back click to close menu
+        super.onBackPressed();
     }
 }
